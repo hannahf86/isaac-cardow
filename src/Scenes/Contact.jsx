5023d244ea5dbdd8d88f14f8eab573d1f2392538
hannahf86 <hannahfeehan.design@gmail.com>
2023-10-25 14:59:54 +0100

    contact form responsive added

--- src/Scenes/Contact.jsx
+++ src/Scenes/Contact.jsx
@@ -6,9 +6,9 @@ const Contact = () => {
     const styles = {
         background: 'h-full bg-zinc-900',
         mobileTitle: 'text-neutral-100 text-5xl text-center pt-12 mb-8 lg:pt-52 tracking-widest',
-        form: 'm-auto text-center',
-        inputName: 'w-11/12 p-2 rounded-md mb-4 border-4 border-sky-800',
-        inputMessage: 'w-11/12 rounded-md h-60 p-2 mb-4 border-4 border-sky-800',
+        formContainer: 'flex flex-col justify-center max-w-xs lg:max-w-2xl md:max-w-xl sm:max-w-lg mx-auto',
+        inputName: 'p-2 rounded-md mb-4 border-4 border-sky-800',
+        inputMessage: 'rounded-md h-60 p-2 mb-4 border-4 border-sky-800',
         button: 'bg-zinc-200 px-6 py-2 rounded-lg border-4 border-sky-800 mt-4',
         buttonText: 'font-light tracking-widest',
     }
@@ -33,30 +33,36 @@ const Contact = () => {
             {/* MOBILE & TABLET */}
             <h1 className={styles.mobileTitle}>CONTACT</h1>
 
-            <div className='lg:mx-60 md:mx-40'>
-                <form ref={form} onSubmit={sendEmail} className={styles.form}>
+
+            <div>
+                <form ref={form} onSubmit={sendEmail} className={styles.formContainer}>
                     <input
                         type='text'
                         name='from_name'
                         placeholder='Full Name'
                         className={styles.inputName}
+                        minLength={2}
                     />
                     <input
                         type='email'
                         name='from_email'
                         placeholder='Email'
                         className={styles.inputName}
+                        maxLength={50}
                     />
                     <input
                         type='textarea'
                         name='message'
                         placeholder='Enter your message'
                         className={styles.inputMessage}
+                        maxLength={500}
                     />
 
-                    <button type="submit" value='Send' className={styles.button}>
-                        <h3 className={styles.buttonText}>SEND</h3>
-                    </button>
+                    <div className='flex justify-center'>
+                        <button type="submit" value='Send' className={styles.button}>
+                            <h3 className={styles.buttonText}>SEND</h3>
+                        </button>
+                    </div>
                 </form>
             </div>
 
