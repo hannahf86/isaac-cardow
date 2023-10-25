f7a9f5190ab400015b39e76cbe00c382e3b06808
hannahf86 <hannahfeehan.design@gmail.com>
2023-10-25 13:06:59 +0100

    images removed from accordion

--- src/Scenes/News.jsx
+++ src/Scenes/News.jsx
@@ -13,7 +13,6 @@ const News = () => {
     const newsData = [
         {
             title: 'RCS Big Guitar Weekend',
-            image: { rcs },
             alt: 'Logo for the Royal Scottish Conservatoire',
             description: `Isaac has been offered a full bursary for The Royal Conservatoire of Scotland's 
                     Big Guitar Weekend in November. The festival includes 2 lessons, classes and 
@@ -24,7 +23,6 @@ const News = () => {
         },
         {
             title: 'Diploma Distinction!',
-            image: { distinction },
             alt: 'MTB Logo and an image of Isaac',
             description: `Isaac achieved a distinction in his Associate Diploma that he took 
                     through the Music Teachers' Board. The report included comments such as "much to admire" and 
@@ -35,7 +33,6 @@ const News = () => {
         },
         {
             title: 'Masterclass with Craig Ogden',
-            image: { craig },
             alt: 'Masterclass with Craig Ogden',
             description: `Isaac took part in a masterclass with world-renowned Craig Ogden at the Chipping 
                         Norton Festival. `,
@@ -43,14 +40,12 @@ const News = () => {
         },
         {
             title: 'Competition Season 2023',
-            image: { trophies },
             alt: 'Various trophy images of Isaac',
             description: `Isaac has been very busy this year, competiting across the country...`,
 
         },
         {
             title: 'Masterclass with Ana Vidovic',
-            image: { ana },
             alt: 'Masterclass with Ana Vidovic',
             description: `As part of the IGF London Guitar Festival 2022, Isaac had a masterclass with the legendary 
         Ana Vidovic...`,
